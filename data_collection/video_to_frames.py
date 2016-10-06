fd7669e6bd61bb7d331303dd350975cdee91e086
Charles Chen <charles.chen@mail.utoronto.ca>
2016-10-06 01:14:52 -0400

    Added simple face detection functionality. This is probably not accurate enough but is a nice prove of concept.

--- data_collection/video_to_frames.py
+++ data_collection/video_to_frames.py
@@ -2,6 +2,8 @@ import os
 import sys
 import argparse
 import re
+import subprocess
+import cv2
 
 # Author: Charles Chen (charles.chen@mail.utoronto.ca)
 # This script requires ffmpeg installed to run!
@@ -14,6 +16,8 @@ parser.add_argument("--num_videos", type=int, default = 10)
 parser.add_argument("--input_type", type=str, default = "gif")
 parser.add_argument("--output_type", type=str, default = "png")
 parser.add_argument("--frame_rate", type=int, default = 10)
+parser.add_argument("--crop_faces", type=bool, default = False)
+parser.add_argument("--facedetect_script_path", type=str, default = "./facedetect.py")
 args = parser.parse_args()
 
 # set parameters
@@ -39,6 +43,12 @@ frame_rate = args.frame_rate
 if (frame_rate <= 0):
     sys.exit("Invalid frame rate.")
 
+crop_faces = args.crop_faces
+if crop_faces:
+    facedetect_script_path = os.path.abspath(args.facedetect_script_path)
+    if not os.path.isfile(facedetect_script_path):
+        sys.exit("facedetec script doesn't exist.")
+
 # main loop
 count = 1
 for file in os.listdir(input_path):
@@ -49,8 +59,29 @@ for file in os.listdir(input_path):
         if not os.path.isdir(output_file_path):
             os.mkdir(output_file_path)
             cmd = " ".join(["ffmpeg -i", input_file_path, "-r", str(frame_rate), os.path.join(output_file_path, "frame_%d." + output_type)])
-            # print cmd
+            print "\n" + cmd
             os.system(cmd)
+
+            if (crop_faces):
+                for image in os.listdir(output_file_path):
+                    image_path = os.path.join(output_file_path, image)
+                    args = ["python", facedetect_script_path, "--best", image_path]
+                    print " ".join(args)
+                    p = subprocess.Popen(args, stdout=subprocess.PIPE)
+                    stdout = p.communicate()[0]
+                    if (stdout != ""): # a face has been detected, crop the image
+                        [x, y, w, h] = [int(i) for i in stdout.split(" ")]
+                        img = cv2.imread(image_path)
+                        cropped_img =  img[y:y+h, x:x+w]
+                        cv2.imwrite(image_path, cropped_img)
+                    else: # no face detected, delete image
+                        os.remove(image_path)
+
+                if os.listdir(output_file_path) == []:
+                    os.rmdir(output_file_path)
+
+                    
+
         
         count += 1
 
