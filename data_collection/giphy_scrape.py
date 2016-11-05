00a6d8ff71ebb499c3ad2645ab8103726abeae76
Charles Chen <charles.chen@mail.utoronto.ca>
2016-11-04 22:54:08 -0400

    Added global set to track downloaded videos druing scraping. Updated keywords record.

--- data_collection/giphy_scrape.py
+++ data_collection/giphy_scrape.py
@@ -13,13 +13,15 @@ giphy_api_search = "http://api.giphy.com/v1/gifs/search?q="
 giphy_api_search_2 = "&api_key=dc6zaTOxFJmzC&limit=100&offset="
 
 # fill in the list keywords
-keyword_list = []
+keyword_list = ["happy"]
 
 def get_videos(threadID, num_items):
     global giphy_api_random
     global giphy_api_search
     global path
     global num_threads
+    global downloaded_videos
+    global lock
 
     i = 0
     failed_counter = 0
@@ -39,6 +41,17 @@ def get_videos(threadID, num_items):
 
                 # set file name to image id
                 file_name = re.search("media/([^/]+)/", video_url).group(1)
+                # check if video with id is already downloaded
+                # if so, skip it
+                # if not, add its id to global set
+                lock.acquire()
+                if (file_name in downloaded_videos):
+                    lock.release()
+                    continue
+                else:
+                    downloaded_videos.add(file_name)
+                    lock.release()
+
                 file_path = os.path.abspath(os.path.join(path, file_name + ".mp4"))
 
                 # retrieve mp4 from query response
@@ -62,12 +75,12 @@ class myThread(threading.Thread):
         print "Starting thread " + str(self.threadID)
         get_videos(self.threadID, self.num_items)
         print "Exiting thread " + str(self.threadID)
-        print "Current Active Count = " + str(threading.active_count())
+        print "Current Active Count = " + str(threading.active_count() - 1)
 
         
 ##### main ######
 parser = argparse.ArgumentParser()
-parser.add_argument("--path", type=str, help="Destination path for storing downloaded videos. Default to \"./mp4s_raw/\".", default = "./mp4s_raw/")
+parser.add_argument("--path", type=str, help="Destination path for storing downloaded videos. Default to \"./data/raw/mp4s_raw/\".", default = "./data/raw/mp4s_raw/")
 parser.add_argument("--num_threads", type=int, help="Number of threads to run. Default to 50.", default = 50)
 parser.add_argument("--num_items_per_thread", type=int, help="Number of videos to be downloaded per thread. Default to 100.", default = 100)
 parser.add_argument("--mode", type=str, help="\"random\" or \"search\". Default to mp4.", default = "random")
@@ -86,6 +99,25 @@ if (num_items_per_thread % 100 != 0):
     sys.exit("Invalid num_items_per_thread. Must be an integer multiple of 100.")
 
 num_threads = args.num_threads
+
+# Global set
+downloaded_videos = set()
+
+# Add downloaded vidoes to global set
+for folder in os.listdir("./data/raw"):
+    dir_path = os.path.join("./data/raw", folder)
+    print dir_path
+    if os.path.isdir(dir_path):
+        for file in os.listdir(dir_path):
+            if file.endswith(".mp4"):
+                video_id = file[:-4]
+                if (video_id in downloaded_videos):
+                    sys.exit("Duplicate!")
+                downloaded_videos.add(video_id)
+
+print "Current dataset size = %d" % len(downloaded_videos)
+
+lock = threading.RLock()
 
 for keyword in keyword_list:
     print "keyword = " + keyword
