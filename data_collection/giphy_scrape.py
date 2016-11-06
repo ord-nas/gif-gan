3af11bec133c7a6fec85320222aa46247c2eaf34
Charles Chen <charles.chen@mail.utoronto.ca>
2016-11-05 21:08:51 -0400

    minor tweaks

--- data_collection/giphy_scrape.py
+++ data_collection/giphy_scrape.py
@@ -12,9 +12,6 @@ giphy_api_random = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
 giphy_api_search = "http://api.giphy.com/v1/gifs/search?q="
 giphy_api_search_2 = "&api_key=dc6zaTOxFJmzC&limit=100&offset="
 
-# fill in the list keywords
-keyword_list = ["happy"]
-
 def get_videos(threadID, num_items):
     global giphy_api_random
     global giphy_api_search
@@ -22,6 +19,10 @@ def get_videos(threadID, num_items):
     global num_threads
     global downloaded_videos
     global lock
+    global new_counter
+    global total_counter
+    global keyword_counter
+    global verbose
 
     i = 0
     failed_counter = 0
@@ -50,13 +51,17 @@ def get_videos(threadID, num_items):
                     continue
                 else:
                     downloaded_videos.add(file_name)
+                    new_counter += 1
+                    total_counter += 1
+                    keyword_counter += 1
                     lock.release()
 
                 file_path = os.path.abspath(os.path.join(path, file_name + ".mp4"))
 
                 # retrieve mp4 from query response
                 urllib.urlretrieve(video_url, file_path)
-                print "\"" + video_url +"\" saved to " + file_path
+                if (verbose):
+                    print "\"" + video_url +"\" saved to " + file_path
             i += 1
             failed_counter = 0
         except:
@@ -72,10 +77,14 @@ class myThread(threading.Thread):
         self.keyword = keyword
     
     def run(self):
-        print "Starting thread " + str(self.threadID)
+        if (verbose):
+            print "Starting thread " + str(self.threadID)
+
         get_videos(self.threadID, self.num_items)
-        print "Exiting thread " + str(self.threadID)
-        print "Current Active Count = " + str(threading.active_count() - 1)
+
+        if (verbose):
+            print "Exiting thread " + str(self.threadID)
+            print "Current Active Slave Count = " + str(threading.active_count() - 2)
 
         
 ##### main ######
@@ -84,6 +93,7 @@ parser.add_argument("--path", type=str, help="Destination path for storing downl
 parser.add_argument("--num_threads", type=int, help="Number of threads to run. Default to 50.", default = 50)
 parser.add_argument("--num_items_per_thread", type=int, help="Number of videos to be downloaded per thread. Default to 100.", default = 100)
 parser.add_argument("--mode", type=str, help="\"random\" or \"search\". Default to mp4.", default = "random")
+parser.add_argument("--verbose", type=bool, help="More output. Default to False.", default = False)
 args = parser.parse_args()
 
 path = args.path
@@ -98,29 +108,51 @@ num_items_per_thread = args.num_items_per_thread
 if (num_items_per_thread % 100 != 0):
     sys.exit("Invalid num_items_per_thread. Must be an integer multiple of 100.")
 
+verbose = args.verbose
+
 num_threads = args.num_threads
 
 # Global set
 downloaded_videos = set()
+new_counter = 0
 
+print "Reading keywords..."
+keyword_list = []
+keyword_file = open("./keywords.txt")
+begin_parse = False
+for line in keyword_file:
+    k = line[:-1] # get rid of newline
+    if (k == "# keywords not tried yet"):
+        begin_parse = True
+        continue
+    elif (begin_parse and k != ""):
+        print "    " + k
+        keyword_list.append(k)
+
+print "\nScanning downloaded videos..."
 # Add downloaded vidoes to global set
 for folder in os.listdir("./data/raw"):
     dir_path = os.path.join("./data/raw", folder)
-    print dir_path
+    print "    scanning: " + dir_path
     if os.path.isdir(dir_path):
         for file in os.listdir(dir_path):
             if file.endswith(".mp4"):
                 video_id = file[:-4]
                 if (video_id in downloaded_videos):
-                    sys.exit("Duplicate!")
+                    sys.exit("    Duplicate! id = " + video_id)
                 downloaded_videos.add(video_id)
 
-print "Current dataset size = %d" % len(downloaded_videos)
+total_counter = len(downloaded_videos)
+
+print "    Current dataset size = %d" % total_counter
+
+print "\nStart scraping..."
 
 lock = threading.RLock()
 
 for keyword in keyword_list:
-    print "keyword = " + keyword
+    print "    Scraping keyword = " + keyword
+    keyword_counter = 0
     thread_list = []
     for t in range(args.num_threads):
         thread = myThread(t, num_items_per_thread, keyword)
@@ -129,6 +161,11 @@ for keyword in keyword_list:
         time.sleep(0.1)
     for thread in thread_list:
         thread.join()
+    print "    New videso obtained for keyword %s = %d" % (keyword, keyword_counter)
+
+print "\nTotal new vidoes obtained = %d" % new_counter
+print "Current dataset size = %d" % total_counter
+print "Done"
     
     
     
