c0639369e7cc004ac8e929cd3e394881b1e87e80
Young  Sandro <youngsan@ug59.eecg>
2017-02-08 00:02:58 -0500

    New discriminator formulation.
    Using discriminator features from one layer back, and then doing a real
    3d convolution instead of flattening and doing 1d convolution.

--- models/recurrent_z/ops.py
+++ models/recurrent_z/ops.py
@@ -61,6 +61,19 @@ def conv2d(input_, output_dim,
 
         return conv
 
+def conv3d(input_, output_dim, 
+           k_d=3, k_h=3, k_w=3, d_d=2, d_h=2, d_w=2, stddev=0.02,
+           name="conv3d"):
+    with tf.variable_scope(name):
+        w = tf.get_variable('w', [k_d, k_h, k_w, input_.get_shape()[-1], output_dim],
+                            initializer=tf.truncated_normal_initializer(stddev=stddev))
+        conv = tf.nn.conv3d(input_, w, strides=[1, d_d, d_h, d_w, 1], padding='SAME')
+
+        biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+        conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+
+        return conv
+
 def deconv2d(input_, output_shape,
              k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
              name="deconv2d", with_w=False):
--- models/recurrent_z/z_model.py
+++ models/recurrent_z/z_model.py
@@ -2,6 +2,7 @@ import os
 from ops import *
 #import scipy.misc
 import numpy as np
+import sys
 
 from model import DCGAN
 from utils import pp
@@ -359,10 +360,8 @@ class VID_DCGAN(object):
         if reuse:
             tf.get_variable_scope().reuse_variables()
 
-        # First we wanna just project into a reasonable shape.
-        # Squeeze all of the per-frame stuff togther
         print "vid:", vid.get_shape().as_list()
-        vid = tf.reshape(vid, [self.batch_size, 1, self.vid_length, -1])
+        vid = tf.reshape(vid, [self.batch_size, self.vid_length, 8, 8, -1])
         num_filters = vid.get_shape().as_list()[-1]
         print "vid (reshaped):", vid.get_shape().as_list()
 
@@ -381,18 +380,16 @@ class VID_DCGAN(object):
         # print "dr0:", layers.dr0.get_shape().as_list()
 
         layers.dr0 = vid
-        layers.dr1 = lrelu(conv2d(layers.dr0, 512, k_h=1, k_w=k_w, d_h=1, name='dvideo_h1'))
+        layers.dr1 = lrelu(conv3d(layers.dr0, 512, name='dvideo_h1'))
         print "dr1:", layers.dr1.get_shape().as_list()
-        layers.dr2 = lrelu(self.d_bn2(conv2d(layers.dr1, 512, k_h=1, k_w=k_w, d_h=1, name='dvideo_h2')))
+        layers.dr2 = lrelu(self.d_bn2(conv3d(layers.dr1, 512, name='dvideo_h2')))
         print "dr2:", layers.dr2.get_shape().as_list()
-        layers.dr3 = lrelu(self.d_bn3(conv2d(layers.dr2, 512, k_h=1, k_w=k_w, d_h=1, name='dvideo_h3')))
+        layers.dr3 = lrelu(self.d_bn3(conv3d(layers.dr2, 512, name='dvideo_h3')))
         print "dr3:", layers.dr3.get_shape().as_list()
-        layers.dr4 = lrelu(self.d_bn4(conv2d(layers.dr3, 512, k_h=1, k_w=k_w, d_h=1, name='dvideo_h4')))
-        print "dr4:", layers.dr4.get_shape().as_list()
-        layers.d5 = linear(tf.reshape(layers.dr4, [self.batch_size, -1]), 1, 'dvideo_h5')
-        print "d5:", layers.d5.get_shape().as_list()
+        layers.d4 = linear(tf.reshape(layers.dr3, [self.batch_size, -1]), 1, 'dvideo_h4')
+        print "d4:", layers.d4.get_shape().as_list()
 
-        return tf.nn.sigmoid(layers.d5), layers.d5, layers
+        return tf.nn.sigmoid(layers.d4), layers.d4, layers
 
 
 def main(_):
